af5d7264440c0288facc0c85ade908baf016f34f
n-haruhi <hr23nakanishi@gmail.com>
2024-02-14 10:32:34 +0000

    [Update]コメント機能実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
 
   has_many :books, dependent: :destroy
   has_many :favorites, dependent: :destroy
+  has_many :book_comments, dependent: :destroy
   has_one_attached :profile_image
 
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
@@ -13,6 +14,6 @@ class User < ApplicationRecord
 
 
   def get_profile_image
-    (profile_image.attached?) ? profile_image : 'no_image.jpg'
+    (profile_image.attached?)? profile_image: 'no_image.jpg'
   end
 end
