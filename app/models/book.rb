307e41d29d045e6ca027c07cdb1bcda9c81954d4
n-haruhi <hr23nakanishi@gmail.com>
2024-02-14 08:37:22 +0000

    [Update]いいね機能実装

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,14 @@
 class Book < ApplicationRecord
   belongs_to :user
+  has_many :favorites, dependent: :destroy
 
   validates :title,presence:true
   validates :body,presence:true,length:{maximum:200}
+
+  # favorited_by?メソッドで、引数で渡されたユーザidがテーブル内に存在exists?するか調べる
+  # trueかfalseを返す
+  def favorited_by?(user)
+    favorites.exists?(user_id: user.id)
+  end
+
 end
